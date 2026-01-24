719e7939e6b2bf579b4bf92d10db1439cf004e1a
guanwei <guanwei.tan.2025@smu.edu.sg>
2026-01-24 18:21:39 +0800

    Initial setup

---
+++ src/client/java/bard/bardrock/BardrockClient.java
@@ -0,0 +1,10 @@
+package bard.bardrock;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BardrockClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
